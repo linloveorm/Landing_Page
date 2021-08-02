e7dc560983be0f8d7dc7641c27f8e7d56b89df69
linloveorm <pinyaratchuenprasertsuk@gmail.com>
2021-08-02 12:04:29 +0700

    start Project

---
+++ my-app/src/Component/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const Footer = () =>{
+
+
+}
+
+export default Footer
---
+++ my-app/src/Component/Header/component/Button_group/Button_Group.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const Button_Group = () =>{
+
+
+}
+
+export default Button_Group
